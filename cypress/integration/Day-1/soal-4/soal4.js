93eb147e224c57b5038c76a4e605b47242ad6a8f
topanrk <47352748+topanrk@users.noreply.github.com>
2021-08-30 22:27:01 +0700

    Soal 4
    Tugas 4

--- cypress/integration/Day-1/soal-4/soal4.js
+++ cypress/integration/Day-1/soal-4/soal4.js
@@ -1,34 +1,25 @@
-describe('Test Product Purchasing', function() {
-    before (()=> {
-        cy.login()
+describe('Test Case Product Store',function(){
+    before(()=>{
+        cy.visit('https://www.demoblaze.com/index.html')
     })
-    it('Cek login', function(){
-        cy.wait(10000)
-        cy.get('#nameofuser').should('have.text','Welcome dede')
-    })
-    it('Purchasing', function(){
-        cy.contains('HTC One M9').click()
-        cy.get('.col-sm-12 > .btn').click()
-        cy.go('back')
-        cy.go('back')
-        cy.contains('Laptops').click()
-        cy.contains('Dell i7 8gb').click()
-        cy.get('.col-sm-12 > .btn').click()
-        cy.go('back')
-        cy.go('back')
-        cy.go('back')
-        cy.contains('Monitors').click()
+
+    it('Pilih Hp', function(){
+        cy.contains('Samsung galaxy s6').click()
         cy.wait(6000)
-        cy.contains('Apple monitor 24').click()
+        cy.get('.name').should('have.text','Samsung galaxy s6')
+        cy.get('.col-sm-12 > .btn').should('have.text','Add to cart')
         cy.get('.col-sm-12 > .btn').click()
-        cy.go('back')
-        cy.go('back')
-        cy.go('back')
+        cy.on('windows:alert',(str)=> {
+            expect(str).to.equal('Product added')
+        })
+    }),
+
+    it('Cek Out',function(){
         cy.get('#cartur').click()
-        cy.get('#tbodyid > :nth-child(1) > :nth-child(2)').should('have.text','Dell i7 8gb')
-        cy.get('#tbodyid > :nth-child(2) > :nth-child(2)').should('have.text','Apple monitor 24')
+        cy.get('.col-lg-1 > h2').should('have.text','Total')
+        cy.wait(6000)
         cy.get('.col-lg-1 > .btn').click()
-        cy.get('#totalm').should('have.text','Total: 1100')
+        // cy.get('#totalm').should('have.text','129560')
         cy.get('#name').type('dudu').should('have.value','dudu')
         cy.get('#country').type('India').should('have.value','India')
         cy.get('#city').type('Delhi').should('have.value','Delhi')
@@ -36,7 +27,6 @@ describe('Test Product Purchasing', function() {
         cy.get('#month').type('Februari').should('have.value','Februari')
         cy.get('#year').type('2022').should('have.value','2022')
         cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click()
-
     })
     Cypress.on('uncaught:exception', (err, runnable)=>{
         return false
